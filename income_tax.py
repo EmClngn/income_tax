e5be59dd421cdaafd555cc818684c77723db6694
unknown <emgcalangian@gmail.com>
2024-01-25 13:31:34 +0800

    pseudo code

---
+++ income_tax.py
@@ -0,0 +1,10 @@
+# Calculate income tax for the given income by adhering to the rules below
+# For example, suppose the taxable income is 45000, and the income tax payable is
+
+# 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+# Pseudo code
+
+# ask user's input for income
+
+# use if statement to calculate the given tax using the given conditions
